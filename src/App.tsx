d68603380951b5e3a6fefd982076c23f13ac6462
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-07 16:24:06 +0600

    создал функцию для закрытия модалки, передал пропсы и контент

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import { useState } from 'react';
 
 const App = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
+
+  const closeModalWindow = () => {
+    setShowModal(!showModal);
+  };
   return (
     <>
-      <Modal show={showModal}/>
+      <Modal show={showModal} title='Order' onClose={closeModalWindow}>
+      modal text content
+      </Modal>
       <button className="btn btn-primary" onClick={() => setShowModal(!showModal)}>Order</button>
     </>
   );
