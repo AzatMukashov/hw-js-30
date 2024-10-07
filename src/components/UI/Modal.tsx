2c7b74771e67b5922e06526987c2fec0b21aab55
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-07 16:23:08 +0600

    добавил пропс для тайтла и кнопку для закрытия модалки

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,21 +1,26 @@
 import Backdrop from './Backdrop.tsx';
 import * as React from 'react';
 
-interface Props {
+interface Props extends React.PropsWithChildren {
   show: boolean;
+  title?: string;
+  onClose: () => void;
 }
 
-const Modal: React.FC<Props> = ({show}) => {
+const Modal: React.FC<Props> = ({show, title = 'Some kinda modal title', children, onClose}) => {
   return (
     <>
       <Backdrop show={show}/>
       <div className="modal show" style={{display: show ? 'block' : 'none'}}>
         <div className="modal-dialog">
           <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5">Modal title</h1>
+            <div className="modal-header justify-content-between">
+              <h1 className="modal-title fs-5">{title}</h1>
+              <button onClick={onClose} className='btn'>X</button>
             </div>
-            Modal Content
+            <div className="p-2">
+            {children}
+            </div>
           </div>
         </div>
       </div>
